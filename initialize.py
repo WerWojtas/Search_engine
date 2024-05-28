6409ad2780e29f2ed951b3c8eedbecd5dbff3295
Weronika Wojtas <weronikawojtas92@gmail.com>
2024-05-28 14:28:34 +0200

    Update initialize.py

--- initialize.py
+++ initialize.py
@@ -2,7 +2,7 @@ from installer import Installer
 
 
 install = Installer()
-#install.scrap()
-#install.parse()
-#install.find_terms()
+install.scrap()
+install.parse()
+install.find_terms()
 install.create_matrixes()
